b8087270e1194054cdae2b5ea88c61f88b93e313
Devajit <patar1447@gmail.com>
2025-08-24 14:12:07 +0530

    Project Setup

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}))
+
+app.use(express.json());
+app.use(cookieParser());
+// app.use(morgan('dev'));
+app.use(morgan());
+app.use(helmet({
+    crossOriginResourcePolicy: false
+}));
+
+const PORT = process.env.PORT || 8080;
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Server is running"
+    })
+});
+
+app.get('/health', (req, res) => {
+    res.json({
+        status: "OK"
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
