2d8a854a402b3d51e6c6371242b43d309d8447a0
Devajit <patar1447@gmail.com>
2025-08-24 19:06:45 +0530

    SubCategory model implemented

---
+++ models/subCategory.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const subCategorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        default: ""
+    },
+    image: {
+        type: String,
+        default: ""
+    },
+    category: [
+        {
+         type: mongoose.Schema.ObjectId,
+         ref: "category", 
+         default: null
+      }
+    ]
+}, {
+    timestamps: true
+});
+
+const SubCategoryModel = mongoose.model("subCategory", subCategorySchema);
+
+export default SubCategoryModel;
